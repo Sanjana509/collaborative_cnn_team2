e39d1a53d6a4f8ae15ab27c932a8d6040663a8fc
Sanjana509 <sanjanasaxena509@gmail.com>
2025-11-19 05:36:06 +0000

    Add model v1 and training metrics

--- models/model_v1.py
+++ models/model_v1.py
@@ -6,15 +6,13 @@ from torchvision import models
 class ModelV1(nn.Module):
     def __init__(self):
         super(ModelV1, self).__init__()
-        # Load MobileNetV2 with default (pretrained) weights
         self.model = models.mobilenet_v2(weights=models.MobileNet_V2_Weights.DEFAULT)
         
-        # Freeze feature layers to make training fast
+
         for param in self.model.features.parameters():
             param.requires_grad = False
             
-        # Replace the classifier head for 2 classes (Cat vs Dog)
-        # MobileNetV2 classifier[1] is the linear layer
+
         in_features = self.model.classifier[1].in_features
         self.model.classifier[1] = nn.Linear(in_features, 2)
 
