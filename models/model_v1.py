65f72a95cc297047025c46dbe4a7d08f74b17b31
Sanjana509 <sanjanasaxena509@gmail.com>
2025-11-20 12:02:40 +0000

    Add model v1 and training metrics

--- models/model_v1.py
+++ models/model_v1.py
@@ -7,11 +7,11 @@ class ModelV1(nn.Module):
     def __init__(self):
         super(ModelV1, self).__init__()
         self.model = models.mobilenet_v2(weights=models.MobileNet_V2_Weights.DEFAULT)
-        
+
 
         for param in self.model.features.parameters():
             param.requires_grad = False
-            
+
 
         in_features = self.model.classifier[1].in_features
         self.model.classifier[1] = nn.Linear(in_features, 2)
